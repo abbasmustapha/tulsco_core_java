0c4ac30a42a9eab16b525493b258a988912feed3
mostafa <abbas.mustapha@gmail.com>
2024-05-19 09:58:37 +0200

    methode overriding

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 https://www.youtube.com/watch?v=4XTsAAHW_Tc
 19/05 5:50:00 heritage
+
+finit 6:36:24 methode overriding
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,27 @@
 package org.example;
 
 
+
+class A {
+    public void show(){
+        System.out.println("I am in A.show()");
+    }
+}
+
+class B extends A{
+    public void show(){
+        System.out.println("I am in B.show()");
+    }
+}
+
+
+
 public class Main {
     public static void main(String[] args) {
 
-        Calc calc = new Calc();
-        AdvCalc advCalc = new AdvCalc();
+        B b = new B();
 
-        int r1 = calc.add(5,6);
-        int r2 = calc.sub(7,3);
-        int r3 = advCalc.multi(7,3);
-        int r4 = advCalc.div(15,4);
-        int r5 = advCalc.add(15,4);
-
-        System.out.println("r1 = " + r1 +"   r2 = "+r2+"   r3 = " + r3 +"   r4 = "+r4+"   r5 = "+r5);
+        b.show();
 
     }
 }
